0508d2a389653549e98c774d9b9e748f8cde08d9
Isabella Ramos <66478873+Isadevs@users.noreply.github.com>
2021-12-16 08:11:57 -0300

    Add files via upload

---
+++ Aula02/exFixacaoIfComposto/exProposto02/exProposto02/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace exProposto02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDoacao = new System.Windows.Forms.TextBox();
+            this.btnInvestimento = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor da Doação";
+            // 
+            // txtDoacao
+            // 
+            this.txtDoacao.Location = new System.Drawing.Point(178, 23);
+            this.txtDoacao.Name = "txtDoacao";
+            this.txtDoacao.Size = new System.Drawing.Size(119, 26);
+            this.txtDoacao.TabIndex = 1;
+            // 
+            // btnInvestimento
+            // 
+            this.btnInvestimento.Location = new System.Drawing.Point(37, 91);
+            this.btnInvestimento.Name = "btnInvestimento";
+            this.btnInvestimento.Size = new System.Drawing.Size(260, 44);
+            this.btnInvestimento.TabIndex = 2;
+            this.btnInvestimento.Text = "Calcular Investimento";
+            this.btnInvestimento.UseVisualStyleBackColor = true;
+            this.btnInvestimento.Click += new System.EventHandler(this.btnInvestimento_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 450);
+            this.Controls.Add(this.btnInvestimento);
+            this.Controls.Add(this.txtDoacao);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDoacao;
+        private System.Windows.Forms.Button btnInvestimento;
+    }
+}
+
---
+++ Aula02/exFixacaoIfComposto/exProposto02/exProposto02/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exProposto02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInvestimento_Click(object sender, EventArgs e)
+        {
+            double valorDoacao, investir;
+
+            valorDoacao = double.Parse(txtDoacao.Text);
+
+            if (valorDoacao <= 1000)
+            {
+               investir =  valorDoacao * 0.05;
+            }
+            else
+            {
+                investir = valorDoacao * 0.15;
+            }
+
+            MessageBox.Show($"O valor do investimento deverá ser de R${investir.ToString("F2")}", "Doação - ONG",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
