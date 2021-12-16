01349c9bf8bee1c4989a4252f9932650ed4ef761
Isabella Ramos <66478873+Isadevs@users.noreply.github.com>
2021-12-16 08:25:33 -0300

    Add files via upload

---
+++ Aula03/exProposto03/exProposto03/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace exProposto03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtBox);
+            this.groupBox1.Location = new System.Drawing.Point(24, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 318);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Caixa do Banco:";
+            // 
+            // txtBox
+            // 
+            this.txtBox.FormattingEnabled = true;
+            this.txtBox.Items.AddRange(new object[] {
+            "1 - Depósito",
+            "2 - Saque",
+            "3 - Ver Saldo"});
+            this.txtBox.Location = new System.Drawing.Point(37, 72);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(267, 28);
+            this.txtBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(267, 70);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Verificar Valor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inserir Valor:";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(37, 141);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(263, 26);
+            this.txtBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Selecione uma Opção:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox txtBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Aula03/exProposto03/exProposto03/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exProposto03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double verSaldo;
+            double deposito = 0;
+            double saque = 0;
+            double saldo = 0;
+
+            switch (txtBox.SelectedIndex)
+            {
+                case 0:
+                    deposito = double.Parse(txtBox1.Text);
+                    MessageBox.Show($" Seu saldo é de R${deposito}");
+                    break;
+                case 1:
+                    deposito = double.Parse(txtBox1.Text);
+                    saque = deposito - saque;
+                    saldo = saque;
+                    MessageBox.Show($"Seu saque R${saque} saldo é de R${saldo}");
+                    break;
+                case 2:
+                    verSaldo = saldo + saque;
+                    MessageBox.Show($"Seu saldo é de R${verSaldo}");
+                    break;
+                default:
+                    MessageBox.Show("Opção Inválida");
+                    break;
+            }
+        }
+    }
+}
