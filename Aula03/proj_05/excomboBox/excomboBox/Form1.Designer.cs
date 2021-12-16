08ba708f25353c982f34e1547b4c1d573338bbb2
Isabella Ramos <66478873+Isadevs@users.noreply.github.com>
2021-12-16 08:32:28 -0300

    Add files via upload

---
+++ Aula03/proj_05/excomboBox/excomboBox/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace excomboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbEscolha = new System.Windows.Forms.ComboBox();
+            this.btnEscolha = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbEscolha
+            // 
+            this.cmbEscolha.FormattingEnabled = true;
+            this.cmbEscolha.Items.AddRange(new object[] {
+            "Ferrari",
+            "Mercedes",
+            "RedBull",
+            "McLaren"});
+            this.cmbEscolha.Location = new System.Drawing.Point(43, 73);
+            this.cmbEscolha.Name = "cmbEscolha";
+            this.cmbEscolha.Size = new System.Drawing.Size(283, 28);
+            this.cmbEscolha.TabIndex = 0;
+            // 
+            // btnEscolha
+            // 
+            this.btnEscolha.Location = new System.Drawing.Point(48, 120);
+            this.btnEscolha.Name = "btnEscolha";
+            this.btnEscolha.Size = new System.Drawing.Size(283, 38);
+            this.btnEscolha.TabIndex = 1;
+            this.btnEscolha.Text = "Confirmar Escolha";
+            this.btnEscolha.UseVisualStyleBackColor = true;
+            this.btnEscolha.Click += new System.EventHandler(this.btnEscolha_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 226);
+            this.Controls.Add(this.btnEscolha);
+            this.Controls.Add(this.cmbEscolha);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbEscolha;
+        private System.Windows.Forms.Button btnEscolha;
+    }
+}
+
---
+++ Aula03/proj_05/excomboBox/excomboBox/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace excomboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEscolha_Click(object sender, EventArgs e)
+        {
+            switch (cmbEscolha.SelectedIndex) //índice selecionado da lista
+            {
+                case 0:
+                    MessageBox.Show("Você selecionou a Equipe Ferrari.");
+                    break;
+                case 1:
+                    MessageBox.Show("Você selecionou a Equipe Mercedes.");
+                    break;
+                case 2:
+                    MessageBox.Show("Você selecionou a Equipe RedBull.");
+                    break;
+                case 3:
+                    MessageBox.Show("Você selecionou a Equipe McLaren.");
+                    break;
+                default:
+                    MessageBox.Show("Selecione uma equipe:");
+                    break;
+            }
+        }
+    }
+}
