567da426b8ddfce9f0b8d9224909109b7194d150
Isabella Ramos <66478873+Isadevs@users.noreply.github.com>
2021-12-16 21:51:22 -0300

    Add files via upload

---
+++ Aula04/proj_somatorio/proj_somatorio/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace proj_somatorio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.AutoSize = true;
+            this.num1.Location = new System.Drawing.Point(33, 39);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(0, 20);
+            this.num1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(317, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Verificar o Valor da soma";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(211, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Valor B:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Valor A";
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Location = new System.Drawing.Point(33, 205);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(86, 20);
+            this.lblRes.TabIndex = 6;
+            this.lblRes.Text = "Resultado:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 244);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label num1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRes;
+    }
+}
+
---
+++ Aula04/proj_somatorio/proj_somatorio/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proj_somatorio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1, n2, soma = 0;
+
+            n1 = int.Parse(textBox1.Text);
+            n2 = int.Parse(textBox2.Text);
+
+            if (n1 < n2)
+            {
+                for(int i = n1 + 1; i < n2; i++)
+                {
+                    soma = i + soma;
+                }
+                lblRes.Text = soma.ToString();
+            }
+            else
+            {
+                MessageBox.Show("O primeiro valor deve ser menor que o Segundo valor");
+            }
+
+        }
+    }
+}
