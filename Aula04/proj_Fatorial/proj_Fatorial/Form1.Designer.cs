e033ce7a531dce5807a24a6cff543857e3392db5
Isabella Ramos <66478873+Isadevs@users.noreply.github.com>
2021-12-16 21:49:11 -0300

    Add files via upload

---
+++ Aula04/proj_Fatorial/proj_Fatorial/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace proj_Fatorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Insira um Valor:";
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(42, 83);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(204, 26);
+            this.txtBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 64);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calcule o Fatorial";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Location = new System.Drawing.Point(42, 239);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(82, 20);
+            this.lblRes.TabIndex = 3;
+            this.lblRes.Text = "Resultado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 307);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblRes;
+    }
+}
+
---
+++ Aula04/proj_Fatorial/proj_Fatorial/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proj_Fatorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int fatorial, multi = 0, i, value;
+
+            fatorial = int.Parse(txtBox.Text);
+            value = fatorial;
+            i = fatorial;
+
+            while(i > 1)
+            {
+                i--;
+                multi = value * i;
+                value = multi;
+            }
+
+            lblRes.Text = multi.ToString();
+        }
+    }
+}
