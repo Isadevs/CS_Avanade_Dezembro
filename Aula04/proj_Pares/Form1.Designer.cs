c3a19ca31bdf2f5c9199f095170eedfbf271c5ff
Isabella Ramos <66478873+Isadevs@users.noreply.github.com>
2021-12-16 21:43:45 -0300

    Add files via upload

---
+++ Aula04/proj_Pares/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace proj_Pares
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtRes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Coloque Dois Números:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(36, 74);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 26);
+            this.txtNum1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Modo Do-While";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(152, 74);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 26);
+            this.txtNum2.TabIndex = 3;
+            // 
+            // txtRes
+            // 
+            this.txtRes.AutoSize = true;
+            this.txtRes.Location = new System.Drawing.Point(32, 211);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(86, 20);
+            this.txtRes.TabIndex = 4;
+            this.txtRes.Text = "Resultado:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 251);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label txtRes;
+    }
+}
+
---
+++ Aula04/proj_Pares/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proj_Pares
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1, n2, value ;
+
+            n1 = int.Parse(txtNum1.Text);
+            n2 = int.Parse(txtNum2.Text);
+            int cont = n1+1;
+            int i = 0;
+
+            if(n1 < n2)
+            {
+
+                do
+                {
+                    if(cont % 2 == 0)
+                    {
+                        i++;
+                    }
+                    cont++;
+                } while (cont < n2);
+                
+                txtRes.Text = i.ToString();
+            }
+            else
+            {
+                MessageBox.Show("O primeiro Número deve ser Menor que o Segundo");
+            }
+        }
+    }
+}
