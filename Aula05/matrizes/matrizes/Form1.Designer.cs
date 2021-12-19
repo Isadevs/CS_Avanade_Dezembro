75f372584ce7802f302171b11400f2130ab5b24f
Isabella Ramos <66478873+Isadevs@users.noreply.github.com>
2021-12-19 15:20:09 -0300

    Add files via upload

---
+++ Aula05/matrizes/matrizes/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace matrizes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.ltlBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Location = new System.Drawing.Point(12, 42);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(270, 53);
+            this.btnSomar.TabIndex = 0;
+            this.btnSomar.Text = "Ver Matriz:";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // ltlBox
+            // 
+            this.ltlBox.FormattingEnabled = true;
+            this.ltlBox.ItemHeight = 20;
+            this.ltlBox.Location = new System.Drawing.Point(12, 125);
+            this.ltlBox.Name = "ltlBox";
+            this.ltlBox.Size = new System.Drawing.Size(270, 144);
+            this.ltlBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 328);
+            this.Controls.Add(this.ltlBox);
+            this.Controls.Add(this.btnSomar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.ListBox ltlBox;
+    }
+}
+
---
+++ Aula05/matrizes/matrizes/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace matrizes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            int[,] matriz = new int[4, 4];
+            int sumLinha = 0;
+            int linhaLoc = 0;
+            double media = 0;
+            double sumMedia = 0;
+            double multi = 0;
+
+
+            //Percorrendo o vetor;
+            for (int linha = 0; linha < 4; linha++)
+            {  
+                for(int coluna = 0; coluna < 4; coluna++)
+                {
+                    linhaLoc = linha + 1;
+                    matriz[linha, coluna] = int.Parse(Interaction.InputBox("Digite um valor para a "+ linhaLoc.ToString()));
+                }           
+            }
+
+            //somar o vetor
+            for (int linha = 0; linha < 1; linha++)
+            {
+                for (int coluna = 0; coluna < 4; coluna++)
+                {
+                    sumLinha = matriz[linha, coluna] + sumLinha;
+                }
+                ltlBox.Items.Add("A Soma da Linha 1 = " + sumLinha.ToString());
+            }
+
+            //media do vetor
+            for(int linha = 1; linha <= 1; linha++)
+            {
+                for(int coluna = 0; coluna < 4; coluna++)
+                {
+                    sumMedia = (double) matriz[linha,coluna] + sumMedia;
+                    media = sumMedia / 4;
+                }
+               ltlBox.Items.Add("A média da Linha 2 = " + media.ToString("F2"));
+            }
+
+            //multiplicação linha 3 com o da mesma coluna linha 4
+            for (int linha = 2; linha <=2; linha++)
+            {
+                for (int coluna = 0; coluna < 4; coluna++)
+                {
+                    int LocLinha = coluna + 1;
+                    multi = matriz[linha, coluna] * matriz[3, coluna];
+                    ltlBox.Items.Add($"Multiplicação da {LocLinha}ª Coluna: {multi.ToString()}");
+                    multi = 0;
+                }          
+            }
+
+        }
+    }
+}
